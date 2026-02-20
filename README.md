e4bf2cf79a19b15bb7ffebd3cdefb2158f9efd58
VincenzoLivigni <enzolivigni13@gmail.com>
2026-02-20 16:44:01 +0100

    Milestone 1 done

---
+++ README.md
@@ -0,0 +1,16 @@
+Milestone 1: Creare un Form con Campi Controllati
+
+✅ - 1 Crea un form di registrazione con i seguenti campi controllati (gestiti con useState):
+    ✔️ - Nome completo (input di testo)
+    ✔️ - Username (input di testo)
+    ✔️ - Password (input di tipo password)
+    ✔️ - Specializzazione (select con opzioni: "Full Stack", "Frontend", "Backend")
+    ✔️ - Anni di esperienza (input di tipo number)
+    ✔️ - Breve descrizione sullo sviluppatore (textarea)
+
+- 2 Aggiungi una validazione al submit, verificando che:
+    ✔️ - Tutti i campi siano compilati
+    ✔️ - L'input Anni di esperienza sia un numero positivo
+    ✔️ - La Specializzazione sia selezionata
+
+✅ - 3 Al submit, se il form è valido, stampa in console i dati.
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,134 @@
+import { useState } from "react"
+
 function App() {
 
+  const [nome, setNome] = useState("")
+  const [cognome, setCognome] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [specializzazione, setSpecializzazione] = useState("")
+  const [esperienza, setEsperienza] = useState("")
+  const [descrizione, setDescrizione] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (nome === "" ||
+      cognome === "" ||
+      username === "" ||
+      password === "" ||
+      specializzazione === "seleziona" ||
+      Number(esperienza) <= 0 ||
+      descrizione === ""
+    ) {
+      console.log("Errore nella compilazione del form")
+    } else {
+      console.log(`Nome: ${nome}`)
+      console.log(`Cognome: ${cognome}`)
+      console.log(`Username: ${username}`)
+      console.log(`Password: ${password}`)
+      console.log(`Specializzazione: ${specializzazione}`)
+      console.log(`Esperienza: ${esperienza}`)
+      console.log(`Descrizione: ${descrizione}`)
+    }
+  }
   return (
     <>
+      <header>
+        <h1 className="text-center">Form di registrazione</h1>
+      </header>
 
+      <main>
+        <div className="container mt-5">
+          <form onSubmit={handleSubmit}>
+            {/* nome */}
+            <section>
+              <label>Nome</label>
+              <input
+                type="text"
+                placeholder="Inserisci nome"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+                required
+              />
+            </section>
+
+            {/* cognome */}
+            <section>
+              <label>Cognome</label>
+              <input
+                type="text"
+                placeholder="Inserisci cognome"
+                value={cognome}
+                onChange={(e) => setCognome(e.target.value)}
+                required
+              />
+            </section>
+
+
+            {/* username */}
+            <section>
+              <label>Username</label>
+              <input
+                type="text"
+                placeholder="Inserisci username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </section>
+
+            {/* password */}
+            <section>
+              <label>Password</label>
+              <input
+                type="password"
+                placeholder="Inserisci password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </section>
+
+            {/* specializzazione */}
+            <section>
+              <label>Specializzazione</label>
+              <select
+                value={specializzazione}
+                onChange={(e) => setSpecializzazione(e.target.value)}>
+
+                <option value="seleziona">Seleziona</option>
+                <option value="full-stack">Full stack</option>
+                <option value="frontend">Frontend</option>
+                <option value="backend">Backend</option>
+              </select>
+            </section>
+
+            {/* esperienza */}
+            <section>
+              <label>Anni di esperienza</label>
+              <input
+                type="number"
+                placeholder="Inserisci anni di esperienza"
+                value={esperienza}
+                onChange={(e) => setEsperienza(e.target.value)}
+                required
+              />
+            </section>
+
+            {/* descrizione */}
+            <section>
+              <label>Descrizione</label>
+              <textarea
+                placeholder="Inserisci una breve descrizione"
+                value={descrizione}
+                onChange={(e) => setDescrizione(e.target.value)}
+                required
+              />
+            </section>
+          </form>
+        </div>
+      </main >
     </>
   )
 }
